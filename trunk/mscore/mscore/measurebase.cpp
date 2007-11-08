196186cb5f609d6119aed1c9d2aa1d3c9426fdf7
wschweer <wschweer@5067e815-4148-4932-9fe1-61ad4153a357>
2007-11-08 10:59:31 +0000

    add missing file

---
+++ trunk/mscore/mscore/measurebase.cpp
@@ -0,0 +1,76 @@
+//=============================================================================
+//  MusE Score
+//  Linux Music Score Editor
+//  $Id:$
+//
+//  Copyright (C) 2002-2007 Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+#include "measurebase.h"
+#include "staff.h"
+
+//---------------------------------------------------------
+//   MeasureBase
+//---------------------------------------------------------
+
+MeasureBase::MeasureBase(Score* score)
+   : Element(score)
+      {
+      _prev = 0;
+      _next = 0;
+      }
+
+//---------------------------------------------------------
+//   collectElements
+//---------------------------------------------------------
+
+void MeasureBase::collectElements(QList<const Element*>& el) const
+      {
+      foreach(Element* e, _el) {
+            if (!e->staff() || e->staff()->show())
+                  el.append(e);
+            }
+      el.append(this);
+      }
+
+//---------------------------------------------------------
+//   add
+//---------------------------------------------------------
+
+/**
+ Add new Element \a el to MeasureBase
+*/
+
+void MeasureBase::add(Element* el)
+      {
+      el->setParent(this);
+      _el.append(el);
+      }
+
+//---------------------------------------------------------
+//   remove
+//---------------------------------------------------------
+
+/**
+ Remove Element \a el from MeasureBase.
+*/
+
+void MeasureBase::remove(Element* el)
+      {
+      if (!_el.remove(el))
+            printf("MeasureBase(%p)::remove(%s,%p) not found\n", this, el->name(), el);
+      }
+
+
