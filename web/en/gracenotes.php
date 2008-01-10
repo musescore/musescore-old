e7880e4dc2a34d9807b2c452c170b6c27be62f90
Werner Schweer <wschweer@users.noreply.github.com>
2008-01-10 18:23:22 +0000

    updates

---
+++ web/en/gracenotes.php
@@ -0,0 +1,22 @@
+<?php
+  $file="gracenotes.php";
+  require("header.html");
+  ?>
+<h4><a href="idx.php">MuseScore</a>
+   -- <a href="manual.php">Reference</a>
+   -- Grace notes</h4>
+
+A short grace note (Acciaccatura) is painted as a small note with a stroke
+through the stem. Its placed before the normal sized main note.
+A long grace note (Appoggiatura) has no stroke.
+<br/>
+<br/>
+Create a grace note by dragging a grace note symbol from the notes palette
+to a main note.
+<br/>
+<br/>
+See also <a href="http://en.wikipedia.org/wiki/Grace_note">
+Grace note</a> in <a href="http://en.wikipedia.org">Wikipedia</a>.
+
+<?php require("trailer.html");  ?>
+
