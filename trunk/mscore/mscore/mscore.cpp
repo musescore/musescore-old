2104f53c2bc45962efff7ff00a5a25a376124726
lvinken <lvinken@5067e815-4148-4932-9fe1-61ad4153a357>
2008-01-13 16:24:46 +0000

    MusicXML updates

--- trunk/mscore/mscore/mscore.cpp
+++ trunk/mscore/mscore/mscore.cpp
@@ -3,7 +3,7 @@
 //  Linux Music Score Editor
 //  $Id: mscore.cpp,v 1.105 2006/09/15 09:34:57 wschweer Exp $
 //
-//  Copyright (C) 2002-2007 Werner Schweer and others
+//  Copyright (C) 2002-2008 Werner Schweer and others
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License version 2.
@@ -1677,6 +1677,8 @@ int main(int argc, char* argv[])
                   }
             else if (fn.endsWith(".xml"))
                   rv = cs->saveXml(fn);
+            else if (fn.endsWith(".mxl"))
+                  rv = cs->saveMxl(fn);
             else if (fn.endsWith(".mid"))
                   rv = cs->saveMidi(fn);
             else if (fn.endsWith(".pdf"))
