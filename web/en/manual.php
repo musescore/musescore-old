106d207dc9bf0b21447887e0f1d67042df59f11e
Werner Schweer <wschweer@users.noreply.github.com>
2008-03-07 15:48:19 +0000

    updates

---
+++ web/en/manual.php
@@ -0,0 +1,14 @@
+<?php
+  $file="manual.php";
+  require("header.html");
+  ?>
+<h4><a href="idx.php">MuseScore</a> -- Documentation</h4>
+
+<a href="reference.php">Online Reference</a>
+<br>
+<br>
+<a href="man-en.pdf">User Manual</a> as PDF file (5 MB) (the manual is
+   outdated and incomplete)
+
+<?php require("trailer.html");  ?>
+
