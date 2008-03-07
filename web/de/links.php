8575a740f9d49e7489b490fd03eaea004f5ed0ae
Werner Schweer <wschweer@users.noreply.github.com>
2008-03-07 15:47:59 +0000

    add files

---
+++ web/de/links.php
@@ -0,0 +1,66 @@
+<?php
+  $file="links.php";
+  require("header.html");
+  ?>
+<!-- Main body -->
+<div class=mainbody>
+
+<h3>Artikel:</h3>
+
+<ul>
+<li><a href="http://www.linuxjournal.com/node/8972/print">Dave Phillips: At
+the Sounding Edge: Music Notation Software, the Final Installment</a>
+</ul>
+
+<h3>Andere freie Notensatzprogramme oder verwandte Programme:</h3>
+
+<ul>
+<li><a href="http://lmuse.sourceforge.net/">MusE</a>
+      Midi/Audio sequencer
+<li><a href="http://lilypond.org">LilyPond</a>
+<li><a href="http://noteedit.berlios.de">NoteEdit</a>
+   ist ein grafisches Frontend für LilyPond von Jörg Anders.</li>
+<li><a href="http://canorus.berlios.de/wiki/index.php/Main_Page">Canorus</a>
+   Nachfolger von NoteEdit</li>
+<li><a href="http://rnvs.informatik.tu-chemnitz.de/~jan/noteedit/noteedit.html">NtEd</a>
+     ist das neue WYSIWYG Notationsprogramm von Jörg Anders.
+<li><a href="http://www-ccrma.stanford.edu/CCRMA/Software/cmn/cmn.html">
+     Common Music Notation</a></li>
+<li><a href="http://rosegardenmusic.com/">Rosegarden</a>
+      Midi/Audio Sequenzer und Notationsprogramm</li>
+<li><a href="http://www.pjb.com.au/muscript/index.html">Muscript</a> ist
+      eine Notensatz-Sprache sowie ein Perlscript, um diese Sprache
+      nach PostScript, Encapsulated PostScript, MIDI oder MusicXML zu übersetzen.
+      </li>
+<li><a href="http://sed.free.fr/gcomposer/">gcomposer</a> a "little tool to
+      write music for Linux".
+      </li>
+</ul>
+
+<br>
+<h3>Tools:</h3>
+<ul>
+   <li><a href="http://fontforge.sourceforge.net/">FontForge(fomerly PfaEdit)</a>
+     ist ein Fonteditor. Der von <i>MuseScore</i> verwendete LilyPond feta Font wurde
+     mit diesem Programm editiert.
+</ul>
+
+
+<br>
+<h3>General:</h3>
+<ul>
+   <li>Dave Phillips <a href="http://linux-sound.org/">linuxsound</a>-page
+      is the ultimate reference for <em>Sound & MIDI Software For Linux</em>.
+   <li>The <a href="http://www.linuxlinks.com/">LinuxLinks</a> Forums;
+      have a look at the <a href="http://www.linuxlinks.com/Software/Multimedia/Music_Notation/">
+      MusicNotation</a> section.
+   <li>Linux-Audio-Development(LAD) Mailing List:<br>
+      <a href="http://lad.linuxaudio.org/">
+      <img src="../pic/ladbutton.gif" width="90" height="31" border="0"
+      alt="Go to the Linux Audio Development Homepage" align="left">
+      </a>
+</ul>
+
+
+<?php require("trailer.html");  ?>
+
