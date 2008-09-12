1eebb38ff1d9ef07575204cb9fdb6389984abf1c
Werner Schweer <wschweer@users.noreply.github.com>
2008-09-12 12:33:10 +0000

    updates

--- web/en/onews.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $file="onews.php";
-  $level="..";
-  require("header.html");
-  ?>
-<h4>MuseScore -- Older News</h4>
-<?php require("../news2.html");  ?>
-<?php require("trailer.html");  ?>
-
