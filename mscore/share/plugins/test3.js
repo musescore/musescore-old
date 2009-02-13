9a1f66bfcd8bfc668772bf67d9b3359f39176708
Werner Schweer <wschweer@users.noreply.github.com>
2009-02-13 22:16:48 +0000

    add another script test file

---
+++ mscore/share/plugins/test3.js
@@ -0,0 +1,66 @@
+//=============================================================================
+//  MuseScore
+//  Linux Music Score Editor
+//  $Id:$
+//
+//  Test plugin
+//
+//  Copyright (C)2008 Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+//
+//    This is ECMAScript code (ECMA-262 aka "Java Script")
+//
+
+//---------------------------------------------------------
+//    init
+//    this function will be called on startup of
+//    mscore
+//---------------------------------------------------------
+
+function init()
+      {
+      // print("test script init");
+      }
+
+//---------------------------------------------------------
+//    run
+//    this function will be called when activating the
+//    plugin menu entry
+//
+//    global Variables:
+//    pluginPath - contains the plugin path; file separator
+//                 is "/"
+//---------------------------------------------------------
+
+function run()
+      {
+      newScore = new Score();
+      newScore.name = "Test-Score";
+      }
+
+//---------------------------------------------------------
+//    menu:  defines were the function will be placed
+//           in the menu structure
+//---------------------------------------------------------
+
+var mscorePlugin = {
+      menu: 'Plugins.CreateScore',
+      init: init,
+      run:  run
+      };
+
+mscorePlugin;
+
