b9927436eb47fe7ebffc59e1e45c1c184b01ab84
wschweer <wschweer@5067e815-4148-4932-9fe1-61ad4153a357>
2009-02-18 09:19:06 +0000

    updates

--- trunk/mscore/mscore/measure.cpp
+++ trunk/mscore/mscore/measure.cpp
@@ -418,6 +418,7 @@ void Measure::layoutChords(Segment* segment, int startTrack, char* tversatz)
       int move1     = notes[startIdx]->staffMove();
       bool mirror   = false;
       int lastHead  = -1;
+      int lastTicks = -1;
       for (int idx = startIdx; idx != endIdx; idx += incIdx) {
             Note* note = notes[idx];
             int move   = note->staffMove();
@@ -429,7 +430,8 @@ void Measure::layoutChords(Segment* segment, int startTrack, char* tversatz)
             if ((note->chord()->isUp() != isLeft) || conflict)
                   isLeft = !isLeft;
             int nmirror   = note->chord()->isUp() != isLeft;
-            bool sameHead = (ll == line) && (head == lastHead);
+            // bool sameHead = (ll == line) && (head == lastHead);
+            bool sameHead = (ll == line) && (ticks == lastTicks);
 
             if (conflict && (nmirror == mirror) && !sameHead) {
                   Note* note = notes[idx];
@@ -461,6 +463,7 @@ void Measure::layoutChords(Segment* segment, int startTrack, char* tversatz)
             move1    = move;
             ll       = line;
             lastHead = head;
+            lastTicks = ticks;
             }
 
       //---------------------------------------------------
