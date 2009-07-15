7ad198a9c4d7c93f9a8619dc34e9cea55befcae8
lasconic <lasconic@5067e815-4148-4932-9fe1-61ad4153a357>
2009-07-15 17:27:11 +0000

    add mscore_en_US.ts file

--- trunk/mscore/gen-qt-projectfile.bat
+++ trunk/mscore/gen-qt-projectfile.bat
@@ -22,6 +22,7 @@ echo       %1/share/locale/mscore_pt.ts \
 echo       %1/share/locale/mscore_sv.ts \
 echo       %1/share/locale/mscore_no.ts \
 echo       %1/share/locale/mscore_en_GB.ts \
+echo       %1/share/locale/mscore_en_US.ts \
 echo       %1/share/locale/mscore_zh_Hant.ts \
 echo       %1/share/locale/mscore_zh_TW.ts \
 echo       %1/share/locale/mscore_uk.ts \
