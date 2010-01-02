08808f235cb11828ec114713b7fd0475035570da
dbolton <dbolton@users.noreply.github.com>
2010-01-02 19:31:29 +0000

    - fix magnification steps - instruments.xml patch from barichd

--- mscore/mscore/scoreview.cpp
+++ mscore/mscore/scoreview.cpp
@@ -2021,14 +2021,12 @@ void ScoreView::zoom(int step, const QPoint& pos)
 
       if (step > 0) {
             for (int i = 0; i < step; ++i) {
-                   _mag += 0.1;
-                  //_mag *= 1.1;
+                   _mag *= 1.1;
                   }
             }
       else {
             for (int i = 0; i < -step; ++i) {
-                  _mag -= 0.1;
-                  // _mag *= 0.9;
+                  _mag /= 1.1;
                   }
             }
       if (_mag > 16.0)
