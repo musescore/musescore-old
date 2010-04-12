23c4008797482300fe5f902c09ad3cacc7604c3e
toemaz <toemaz@5067e815-4148-4932-9fe1-61ad4153a357>
2010-04-12 14:43:03 +0000

    Adding missing file

---
+++ trunk/mscore/share/locale/qt_zh_CN.ts
@@ -0,0 +1,110 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+  <link rel="icon" href="http://www.qtsoftware.com/favicon.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="http://www.qtsoftware.com/favicon.ico" type="image/x-icon" />
+  
+<meta http-equiv="X-UA-Compatible" content="chrome=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>translations/qt_zh_CN.ts - widgets-ng in Qt Labs - Qt by Nokia</title>
+<link href="/stylesheets/base.css?1269594223" media="screen" rel="stylesheet" type="text/css" /><script src="/javascripts/all.js?1269594223" type="text/javascript"></script>      <link href="/stylesheets/prettify/prettify.css?1269594223" media="screen" rel="stylesheet" type="text/css" />    <script src="/javascripts/prettify/prettify.js?1269594223" type="text/javascript"></script>        <script type="text/javascript" charset="utf-8">
+      $(document).ready(function(){
+          if (parseInt($("#codeblob tr td.line-numbers:last").text()) < 3500) {
+              prettyPrint();
+          } else {
+              $("#long-file").show().find("a#highlight-anyway").click(function(e){
+                  prettyPrint();
+                  e.preventDefault();
+              });
+          }
+      });
+    </script>
+  
+  <link href="/stylesheets/sites/qt/qt.css?1269594228" media="screen" rel="stylesheet" type="text/css" /></head>
+
+<body id="blobs">
+    <div id="header">
+    <h1 id="logo">
+      <a href="/"><img alt="Qt-logo" src="/images/sites/qt/qt-logo.png?1269594228" /></a> 
+    </h1>
+    
+    <ul id="menu">
+              <li><a href="/">Home</a></li>
+                  
+    	<li class="secondary"><a href="/users/new">Register</a></li>
+    <li class="secondary"><a href="/login">Login</a></li>
+    </ul>
+    
+    <div id="searchbox">
+      
+
+
+<div class="search_bar">
+<form action="http://gitorious.org/search" method="get"><p>
+  <input class="text search-field round-5" id="q" name="q" type="text" /> 
+  <input type="submit" value="Search" class="search-submit round-5" />
+</p>  
+<p class="hint search-hint">
+  eg. 'wrapper', 'category:python' or '"document database"'
+    - <small><a id="advanced-search-toggler" href="#advanced">Advanced search tips</a></small>
+  </p>
+</form></div>
+    </div>
+    
+	  </div>
+    <ul id="breadcrumbs">
+    <li class="project"><a href="/qt-labs">Qt Labs</a></li><li class="repository"><a href="/qt-labs/widgets-ng">widgets-ng</a></li><li class="branch"><a href="/qt-labs/widgets-ng/commits/master">master</a></li><li class="tree"><a href="/qt-labs/widgets-ng/trees/master">/</a></li><li class="folder"><a href="/qt-labs/widgets-ng/trees/master/translations">translations</a></li><li class="file"><a href="/qt-labs/widgets-ng/blobs/master/translations/qt_zh_CN.ts">qt_zh_CN.ts</a></li>  </ul>
+    <div id="container" class="">
+    <div id="content" class="">
+      
+      
+
+
+
+
+<!-- mime: text/plain -->
+
+  <p>
+	This blob appears to be binary data, if you like you can <a href="/qt-labs/widgets-ng/blobs/raw/a28262e21a9ce4a4df05d4211a9f4b60a8f4337f/translations/qt_zh_CN.ts">download the raw data</a> (right click, save as)
+  </p>
+
+
+
+<div class="page-meta">
+  <ul class="page-actions">
+    <li>Blob contents</li>
+    <li><a href="/qt-labs/widgets-ng/blobs/history/master/translations/qt_zh_CN.ts">Blob history</a></li>
+    <li><a href="/qt-labs/widgets-ng/blobs/raw/master/translations/qt_zh_CN.ts">Raw blob data</a></li>
+  </ul>
+</div>    </div>
+        <div id="footer">
+      
+<div class="powered-by">
+  <a href="http://gitorious.org"><img alt="Powered_by_gitorious" src="/images/powered_by_gitorious.png?1269594223" title="Powered by Gitorious" /></a></div>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-52238-3']);
+_gaq.push(['_setDomainName', '.gitorious.org'])
+_gaq.push(['_trackPageview']);
+(function() {
+   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+})();
+</script>      <ul>
+        <li><a href="/">Home</a> | </li>
+        <li><a href="/about/faq">FAQ</a> | </li>
+        <li><a href="/contact">Contact</a> | </li>
+        <li><a href="http://qt.nokia.com/">Qt by Nokia</a></li>
+      </ul>
+              <ul class="legal">
+          <li><a href="http://en.gitorious.org/tos">Terms of Service</a> | </li>
+          <li><a href="http://en.gitorious.org/privacy_policy">Privacy Policy</a></li>
+        </ul>
+          <div class="clear"></div>
+  </div>
+</body>
+</html>
