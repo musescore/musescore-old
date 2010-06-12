befe0e6789cb8b7a51af282566fd964da8f26a8f
lasconic <lasconic@5067e815-4148-4932-9fe1-61ad4153a357>
2010-06-12 09:29:19 +0000

    patch by Aaron Epstein for #5923 : Transpose chordnames does not preserve pitch spelling

--- branches/mscore-0-9-6/mscore/mscore/utils.cpp
+++ branches/mscore-0-9-6/mscore/mscore/utils.cpp
@@ -519,48 +519,8 @@ int searchInterval(int steps, int semitones)
 void transposeInterval(int pitch, int tpc, int* rpitch, int* rtpc, Interval interval,
    bool useDoubleSharpsFlats)
       {
-      *rpitch   = pitch + interval.chromatic;
-      int steps = interval.diatonic;
-
-      int minAlter;
-      int maxAlter;
-      if (useDoubleSharpsFlats) {
-            minAlter = -2;
-            maxAlter = 2;
-            }
-      else {
-            minAlter = -1;
-            maxAlter = 1;
-            }
-
-      int step, alter;
-      for (;;) {
-            int octave = (pitch / 12);
-
-            step = tpc2step(tpc) + steps;
-            while (step < 0) {
-                  step += 7;
-                  octave -= 1;
-                  }
-            while (step >= 7) {
-                  step -= 7;
-                  octave += 1;
-                  }
-
-            int p1     = tpc2pitch(step2tpc(step, 0)) + octave * 12;
-            alter      = interval.chromatic - (p1 - pitch);
-            if (alter < 0)
-                  alter = -((-alter) % 12);
-            else
-                  alter %= 12;
-            if (alter > maxAlter)
-                  ++steps;
-            else if (alter < minAlter)
-                  --steps;
-            else
-                  break;
-            }
-      *rtpc  = step2tpc(step, alter);
+      *rpitch = pitch + interval.chromatic;
+      *rtpc   = transposeTpc(tpc, interval, useDoubleSharpsFlats);
       }
 
 //---------------------------------------------------------
@@ -599,17 +559,25 @@ int transposeTpc(int tpc, Interval interval, bool useDoubleSharpsFlats)
             int p1 = tpc2pitch(step2tpc(step, 0));
             alter  = semitones - (p1 - pitch);
             // alter  = p1 + semitones - pitch;
+#if 0
             if (alter < 0)
                   alter = -((-alter) % 12);
             else
                   alter %= 12;
+#else
+            alter %= 12;
+            if (alter > 6)
+                  alter -= 12;
+#endif
             if (alter > maxAlter)
                   ++steps;
             else if (alter < minAlter)
                   --steps;
             else
                   break;
+            printf("  again alter %d steps %d, step %d\n", alter, steps, step);
             }
+      printf("  = step %d alter %d  tpc %d\n", step, alter, step2tpc(step, alter));
       return step2tpc(step, alter);
       }
 
