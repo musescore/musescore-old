e13efb50216e9810c260ef04441dc933c8fbee1a
wschweer <wschweer@5067e815-4148-4932-9fe1-61ad4153a357>
2010-06-19 13:19:48 +0000

    add missing files

---
+++ trunk/mscore/mscore/staffstate.h
@@ -0,0 +1,56 @@
+//=============================================================================
+//  MusE Score
+//  Linux Music Score Editor
+//  $Id:$
+//
+//  Copyright (C) 2010 Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+#ifndef __STAFFSTATE_H__
+#define __STAFFSTATE_H__
+
+#include "element.h"
+
+// staff state change subtypes:
+
+enum { STAFF_STATE_INSTRUMENT, STAFF_STATE_TYPE, STAFF_STATE_VISIBLE };
+
+//---------------------------------------------------------
+//   StaffState
+//---------------------------------------------------------
+
+class StaffState : public Element {
+      qreal lw;
+      QPainterPath path;
+
+      virtual void draw(QPainter&, ScoreView*) const;
+      virtual void layout();
+
+   public:
+      StaffState(Score*);
+      virtual StaffState* clone() const { return new StaffState(*this); }
+      virtual ElementType type() const   { return STAFF_STATE; }
+      virtual void setSubtype(const QString&);
+      virtual void setSubtype(int st)    { Element::setSubtype(st); }
+      virtual const QString subtypeName() const;
+      virtual bool acceptDrop(ScoreView*, const QPointF&, int, int) const;
+      virtual Element* drop(ScoreView*, const QPointF&, const QPointF&, Element*);
+      virtual bool genPropertyMenu(QMenu*) const;
+      virtual void propertyAction(ScoreView*, const QString&);
+      virtual void write(Xml&) const;
+      virtual void read(QDomElement);
+      };
+
+#endif
