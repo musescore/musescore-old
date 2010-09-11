93c7be8610c0c9602fe9e04edd755bb311d8b2d1
wschweer <wschweer@5067e815-4148-4932-9fe1-61ad4153a357>
2010-09-11 16:54:08 +0000

    add files

---
+++ trunk/mscore/mstyle/mconfig.cpp
@@ -0,0 +1,45 @@
+//=============================================================================
+//  MuseScore
+//  Linux Music Score Editor
+//  $Id:$
+//
+//  Copyright (C) 2010 Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+#include "mconfig.h"
+
+bool MStyleConfigData::genericAnimationsEnabled = true;
+bool MStyleConfigData::animationsEnabled = true;
+bool MStyleConfigData::progressBarAnimationsEnabled = true;
+bool MStyleConfigData::progressBarAnimated = true;
+
+int  MStyleConfigData::menuBarAnimationType = MStyleConfigData::MB_FADE;
+int  MStyleConfigData::menuAnimationType = MStyleConfigData::ME_FADE;
+
+int  MStyleConfigData::genericAnimationsDuration;
+int  MStyleConfigData::toolBarAnimationsDuration;
+int  MStyleConfigData::menuAnimationsDuration;
+int  MStyleConfigData::progressBarAnimationsDuration;
+int  MStyleConfigData::progressBarBusyStepDuration;
+int  MStyleConfigData::menuBarAnimationsDuration;
+int  MStyleConfigData::menuFollowMouseAnimationsDuration;
+int  MStyleConfigData::menuBarFollowMouseAnimationsDuration;
+bool MStyleConfigData::menuBarAnimationsEnabled;
+bool MStyleConfigData::menuAnimationsEnabled;
+int  MStyleConfigData::toolBarAnimationType;
+
+int  MStyleConfigData::menuHighlightMode = MStyleConfigData::MM_DARK;
+bool MStyleConfigData::tabSubtleShadow = true;
+
