8ac95140e4b14b430b37a3782641256e95e26d11
miwarre <miwarre@5067e815-4148-4932-9fe1-61ad4153a357>
2010-12-12 22:36:28 +0000

    Tabulature internal fonts were not loaded.

--- trunk/mscore/mscore/mscore.cpp
+++ trunk/mscore/mscore/mscore.cpp
@@ -1999,7 +1999,8 @@ int main(int argc, char* av[])
       static const char* fonts[] = {
 //            "mscore-20", "mscore1-20", "MuseJazz", "DejaVuSans", "DejaVuSerif"
             "mscore-20", "mscore1-20", "MuseJazz", "FreeSans", "FreeSerif",
-            "gonville-20"
+            "gonville-20",
+            "mscore_tab_baroque", "mscore_tab_modern", "mscore_tab_renaiss", "mscore_tab_renaiss2"
             };
       for (unsigned i = 0; i < sizeof(fonts)/sizeof(*fonts); ++i) {
             if (-1 == QFontDatabase::addApplicationFont(QString(":/fonts/%1.ttf").arg(fonts[i]))) {
