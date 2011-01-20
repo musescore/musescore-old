4ecd9c5e6c0cc67251d78f424c2ad0da69eb4ed4
lasconic <lasconic@5067e815-4148-4932-9fe1-61ad4153a357>
2011-01-20 16:06:30 +0000

    more fix #6599: Command line option to change config/settings directory

--- trunk/mscore/mscore/mscore.cpp
+++ trunk/mscore/mscore/mscore.cpp
@@ -1946,6 +1946,7 @@ int main(int argc, char* av[])
                         if (dir.exists(path)) {
                               QSettings::setPath(QSettings::IniFormat, QSettings::UserScope, path);
                               QSettings::setPath(QSettings::IniFormat, QSettings::SystemScope, path);
+                              dataPath = path;
                               }
                         }
                         break;
@@ -1978,7 +1979,8 @@ int main(int argc, char* av[])
             }
 
 /**/
-      dataPath = QDesktopServices::storageLocation(QDesktopServices::DataLocation);
+      if(dataPath.isEmpty())
+            dataPath = QDesktopServices::storageLocation(QDesktopServices::DataLocation);
 
       // create local plugin directory
       // if not already there:
