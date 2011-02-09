719b912eb600c037bf5a104b87f4955dae24dadf
lasconic <lasconic@5067e815-4148-4932-9fe1-61ad4153a357>
2011-02-09 16:45:27 +0000

    fix image path hack

--- branches/mscore-0-9-6/mscore/mscore/scoreview.cpp
+++ branches/mscore-0-9-6/mscore/mscore/scoreview.cpp
@@ -1913,12 +1913,7 @@ void ScoreView::dropEvent(QDropEvent* event)
                   else
                         return;
                   _score->startCmd();
-                  QString str(u.path());
-                  #if defined(Q_WS_WIN)
-                  if (str.startsWith("/"))    // HACK
-                        str = str.mid(1);
-                  #endif
-                  s->setPath(str);
+                  s->setPath(u.toLocalFile());
 if (debugMode)
       printf("drop image <%s> <%s>\n", qPrintable(str), qPrintable(s->path()));
 
