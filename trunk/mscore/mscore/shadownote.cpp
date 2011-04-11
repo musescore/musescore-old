dcd58ab0e3746111928bedb1d461016e8a486a94
wschweer <wschweer@5067e815-4148-4932-9fe1-61ad4153a357>
2011-04-11 18:22:11 +0000

    fix #7814

--- trunk/mscore/mscore/shadownote.cpp
+++ trunk/mscore/mscore/shadownote.cpp
@@ -53,7 +53,7 @@ void ShadowNote::draw(Painter* painter) const
       qreal lw = point(score()->styleS(ST_ledgerLineWidth));
       InputState ps = score()->inputState();
       int voice;
-      if (ps.drumNote() != -1 && ps.drumset())
+      if (ps.drumNote() != -1 && ps.drumset() && ps.drumset()->isValid(ps.drumNote()))
             voice = ps.drumset()->voice(ps.drumNote());
       else
             voice = ps.voice();
