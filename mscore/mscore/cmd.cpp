26f0b14637c034b7ccddf08541af788ea68ff865
Nicolas Froment <lasconic@users.noreply.github.com>
2011-04-17 21:12:13 +0000

    fix #10264: Select a flag and enter a note causes crash

--- mscore/mscore/cmd.cpp
+++ mscore/mscore/cmd.cpp
@@ -413,6 +413,8 @@ void Score::cmdRemove(Element* e)
 void ScoreView::cmdAddPitch(int note, bool addFlag)
       {
       InputState& is = _score->inputState();
+      if (is.track == -1)          // invalid state
+            return;
       Drumset* ds    = is.drumset();
       int pitch;
       if (ds) {
