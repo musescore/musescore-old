83a062934b8f10f55e7a46780b747cd4002f3619
lasconic <lasconic@5067e815-4148-4932-9fe1-61ad4153a357>
2011-06-02 11:22:27 +0000

    Rename WRC to WRC.BAT in CMakeList.txt

--- branches/mscore-0-9-6/mscore/cmake/mingw32.mingw.cmake
+++ branches/mscore-0-9-6/mscore/cmake/mingw32.mingw.cmake
@@ -36,6 +36,9 @@ set (QT_UIC_EXECUTABLE        "${CROSSQT}/bin/uic.exe")
 set (QT_RCC_EXECUTABLE        "${CROSSQT}/bin/rcc")
 set (QT_QTCORE_LIBRARY        "mops")
 
+set (QT_WRC_EXECUTABLE        wrc.bat)
+set (QT_WINE_EXECUTABLE       wine.bat)
+
 set (QT_INCLUDES ${QT_INCLUDE_DIR} ${QT_QT_INCLUDE_DIR}
      ${QT_QTCORE_INCLUDE_DIR} ${QT_QTXML_INCLUDE_DIR} ${QT_GUI_INCLUDE_DIR}
      ${QT_QTNETWORK_INCLUDE_DIR}
