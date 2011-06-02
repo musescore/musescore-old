1fd691a440926294fee5c79bc27d85a3aa27e079
lasconic <lasconic@5067e815-4148-4932-9fe1-61ad4153a357>
2011-06-02 11:21:09 +0000

    Rename WRC to WRC.BAT in CMakeList.txt

--- trunk/mscore/cmake/mingw32.mingw.cmake
+++ trunk/mscore/cmake/mingw32.mingw.cmake
@@ -27,7 +27,7 @@ set (QT_QTXML_INCLUDE_DIR     ${CROSSQT}/include/QtXml)
 set (QT_QTGUI_INCLUDE_DIR     ${CROSSQT}/include/QtGui)
 set (QT_QTNETWORK_INCLUDE_DIR ${CROSSQT}/include/QtNetwork)
 set (QT_QTUITOOLS_INCLUDE_DIR ${CROSSQT}/include/QtUiTools)
-set (QT_QTSCRIPT_INCLUDE_DIR ${CROSSQT}/include/QtScript)
+set (QT_QTSCRIPT_INCLUDE_DIR  ${CROSSQT}/include/QtScript)
 set (QT_LIBRARY_DIR           ${CROSSQT}/lib)
 
 set (QT_MOC_EXECUTABLE        "${CROSSQT}/bin/moc.exe")
@@ -35,6 +35,10 @@ set (QT_UIC_EXECUTABLE        "${CROSSQT}/bin/uic.exe")
 ## set (QT_UIC_EXECUTABLE        "/usr/bin/uic-qt4")
 set (QT_RCC_EXECUTABLE        "${CROSSQT}/bin/rcc")
 set (QT_QTCORE_LIBRARY        "mops")
+
+set (QT_WRC_EXECUTABLE        wrc.bat)
+set (QT_WINE_EXECUTABLE       wine.bat)
+
 
 set (QT_INCLUDES ${QT_INCLUDE_DIR} ${QT_QT_INCLUDE_DIR}
      ${QT_QTCORE_INCLUDE_DIR} ${QT_QTXML_INCLUDE_DIR} ${QT_GUI_INCLUDE_DIR}
