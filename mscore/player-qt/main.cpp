84ca7c6d9be602d4ebb55d55ddb15eaa13a8588b
Werner Schweer <wschweer@users.noreply.github.com>
2011-06-18 13:37:15 +0000

    updates

---
+++ mscore/player-qt/main.cpp
@@ -0,0 +1,38 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//  $Id:$
+//
+//  Copyright (C) 2011 Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2
+//  as published by the Free Software Foundation and appearing in
+//  the file LICENSE.GPL
+//=============================================================================
+
+#include <stdio.h>
+#include "libmscore/mscore.h"
+#include "scoreview.h"
+
+bool debugMode = false;
+
+//---------------------------------------------------------
+//   main
+//---------------------------------------------------------
+
+int main(int argc, char* argv[])
+      {
+      if (argc != 2) {
+            fprintf(stderr, "usage: %s <scorefile>\n", argv[0]);
+            return -1;
+            }
+      MScore::init();
+      new QApplication(argc, argv);
+
+      ScoreView* view = new ScoreView;
+//      view->loadFile(argv[1]);
+      view->show();
+      return qApp->exec();
+      }
+
---
+++ mscore/player-qt/scoreview.h
@@ -0,0 +1,31 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//  $Id:$
+//
+//  Copyright (C) 2011 Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2
+//  as published by the Free Software Foundation and appearing in
+//  the file LICENSE.GPL
+//=============================================================================
+
+#ifndef __SCOREVIEW_H__
+#define __SCOREVIEW_H__
+
+
+//---------------------------------------------------------
+//   ScoreView
+//---------------------------------------------------------
+
+class ScoreView : public QWidget {
+      Q_OBJECT
+
+   public:
+      ScoreView(QWidget* parent = 0) : QWidget(parent) {}
+      };
+
+
+#endif
+
