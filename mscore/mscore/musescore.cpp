bff0bbaf521abdf189e9d4fa69c588b654e6daae
Nicolas Froment <lasconic@users.noreply.github.com>
2011-07-14 10:45:33 +0000

    remove version number in default folder in startup menu on Windows fix tmp dir for musescore connect

--- mscore/mscore/musescore.cpp
+++ mscore/mscore/musescore.cpp
@@ -3845,7 +3845,7 @@ void MuseScore::networkFinished(QNetworkReply* reply)
       printf("name <%s>\n", qPrintable(name));
 
       QByteArray data = reply->readAll();
-      QString tmpName = "/tmp/" + name;
+      QString tmpName = QDir::tempPath () + "/"+ name;
       QFile f(tmpName);
       f.open(QIODevice::WriteOnly);
       f.write(data);
