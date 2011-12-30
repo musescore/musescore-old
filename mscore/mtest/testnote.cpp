2ee2b42a71a1315b5896d6d794aa14e306c1d63c
Werner Schweer <wschweer@users.noreply.github.com>
2011-12-30 15:06:27 +0000

    update

--- mscore/mtest/testnote.cpp
+++ mscore/mtest/testnote.cpp
@@ -195,6 +195,145 @@ bool testNote()
       TEST(n->veloType() == AUTO_VAL);
       delete n;
 
+      //================================================
+      //   test setProperty(int, QVariant)
+      //================================================
+
+   // pitch
+      note->setProperty(P_PITCH, 32);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->pitch() == 32);
+      delete n;
+
+   // tpc
+      note->setProperty(P_TPC, 21);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->tpc() == 21);
+      delete n;
+
+   // small
+      note->setProperty(P_SMALL, false);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(!n->small());
+      delete n;
+
+      note->setProperty(P_SMALL, true);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->small());
+      delete n;
+
+   // mirror
+      note->setProperty(P_MIRROR_HEAD, int(DH_LEFT));
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->userMirror() == DH_LEFT);
+      delete n;
+
+      note->setProperty(P_MIRROR_HEAD, int(DH_RIGHT));
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->userMirror() == DH_RIGHT);
+      delete n;
+
+      note->setProperty(P_MIRROR_HEAD, DH_AUTO);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->userMirror() == DH_AUTO);
+      delete n;
+
+   // dot position
+      note->setProperty(P_DOT_POSITION, UP);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->dotPosition() == UP);
+      delete n;
+
+      note->setProperty(P_DOT_POSITION, DOWN);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->dotPosition() == DOWN);
+      delete n;
+
+      note->setProperty(P_DOT_POSITION, AUTO);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->dotPosition() == AUTO);
+      delete n;
+
+  // onTimeUserOffset
+      note->setProperty(P_ONTIME_OFFSET, 9);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->onTimeUserOffset() == 9);
+      delete n;
+
+  // offTimeUserOffset
+      note->setProperty(P_OFFTIME_OFFSET, 19);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->offTimeUserOffset() == 19);
+      delete n;
+
+  // headGroup
+      for (int i = 0; i < HEAD_GROUPS; ++i) {
+            note->setProperty(P_HEAD_GROUP, int(i));
+            n = static_cast<Note*>(writeReadElement(note));
+            TEST(n->headGroup() == i);
+            delete n;
+            }
+
+  // headType
+      for (int i = 0; i < 5; ++i) {
+            note->setProperty(P_HEAD_TYPE, i);
+            n = static_cast<Note*>(writeReadElement(note));
+            TEST(n->headType() == i);
+            delete n;
+            }
+
+   // velo offset
+      note->setProperty(P_VELO_OFFSET, 38);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->veloOffset() == 38);
+      delete n;
+
+   // tuning
+      note->setProperty(P_TUNING, 2.4);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->tuning() == 2.4);
+      delete n;
+
+   // fret
+      note->setProperty(P_FRET, 7);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->fret() == 7);
+      delete n;
+
+   // string
+      note->setProperty(P_STRING, 4);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->string() == 4);
+      delete n;
+
+   // ghost
+      note->setProperty(P_GHOST, false);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(!n->ghost());
+      delete n;
+
+      note->setProperty(P_GHOST, true);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->ghost());
+      delete n;
+
+   // velo type
+      note->setProperty(P_VELO_TYPE, USER_VAL);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->veloType() == USER_VAL);
+      delete n;
+
+      note->setProperty(P_VELO_TYPE, OFFSET_VAL);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->veloType() == OFFSET_VAL);
+      delete n;
+
+      note->setProperty(P_VELO_TYPE, AUTO_VAL);
+      n = static_cast<Note*>(writeReadElement(note));
+      TEST(n->veloType() == AUTO_VAL);
+      delete n;
+
+
       return passed;
       }
 
