6327fde4eee9bd0aa3702193469cf0d7044c1f1c
wschweer <wschweer@5067e815-4148-4932-9fe1-61ad4153a357>
2011-12-31 10:46:37 +0000

    for omr switch from poppler to muPDF

--- trunk/mscore/omr/pdf.cpp
+++ trunk/mscore/omr/pdf.cpp
@@ -19,180 +19,14 @@
 //=============================================================================
 
 #include "pdf.h"
-#include "poppler/PDFDoc.h"
-#include "poppler/OutputDev.h"
-#include "poppler/GlobalParams.h"
-#include "poppler/GfxState.h"
-
-//------------------------------------------------------------------------
-// QImageOutputDev
-//------------------------------------------------------------------------
-
-class QImageOutputDev: public OutputDev {
-      QImage* image;
-      int ny;
-
-   public:
-      QImageOutputDev()           { image = 0;   }
-      void setImage(QImage* img)  { image = img; }
-      virtual ~QImageOutputDev()  {}
-
-      virtual GBool interpretType3Chars() { return gFalse; }
-      virtual GBool needNonText()         { return gTrue; }
-
-      // 0,0 is top left corner
-      virtual GBool upsideDown()          { return gFalse; }
-
-      virtual GBool useDrawChar()         { return gFalse; }
-
-  //----- image drawing
-      virtual void drawImage(GfxState *state, Object *ref, Stream *str,
-	   int width, int height, GfxImageColorMap *colorMap,
-	   GBool interpolate, int *maskColors, GBool inlineImg);
-      virtual void drawMaskedImage(GfxState *state, Object *ref, Stream *str,
-         int width, int height,
-	   GfxImageColorMap *colorMap,
-	   GBool interpolate,
-	   Stream *maskStr, int maskWidth, int maskHeight,
-	   GBool maskInvert, GBool maskInterpolate);
-      virtual void drawSoftMaskedImage(GfxState *state, Object *ref, Stream *str,
-	   int width, int height,
-	   GfxImageColorMap *colorMap,
-	   GBool interpolate,
-	   Stream *maskStr,
-	   int maskWidth, int maskHeight,
-	   GfxImageColorMap *maskColorMap,
-	   GBool maskInterpolate);
-      };
-
-void QImageOutputDev::drawMaskedImage(GfxState *state, Object *ref, Stream *str,
-         int width, int height,
-	   GfxImageColorMap *colorMap,
-	   GBool interpolate,
-	   Stream *maskStr, int maskWidth, int maskHeight,
-	   GBool maskInvert, GBool maskInterpolate)
-      {
-      printf("=========drawMaskedImage\n");
-      abort();
+extern "C" {
+#include <fitz.h>
+#include <mupdf.h>
       }
 
-void QImageOutputDev::drawSoftMaskedImage(GfxState *state, Object *ref, Stream *str,
-	   int width, int height,
-	   GfxImageColorMap *colorMap,
-	   GBool interpolate,
-	   Stream *maskStr,
-	   int maskWidth, int maskHeight,
-	   GfxImageColorMap *maskColorMap,
-	   GBool maskInterpolate)
-      {
-      printf("==========drawSoftMaskedImage\n");
-      abort();
-      }
-
-//---------------------------------------------------------
-//   drawImage
-//---------------------------------------------------------
-
-void QImageOutputDev::drawImage(GfxState* state, Object*, Stream* str,
-   int width, int height, GfxImageColorMap* colorMap, GBool, int*, GBool)
-      {
-      if (colorMap->getNumPixelComps() == 1 && colorMap->getBits() == 1) {
-            double* ctm = state->getCTM();
-            double xmag = width/ctm[0];
-            double ymag = height/ctm[3];
-            double xoff = (ctm[2]+ctm[4]) * xmag;
-            int ph      = state->getPageHeight() * ymag;
-            int yoff    = int(ph - (ctm[3]+ctm[5]) * ymag);
-
-            //yoff needs to be > 0
-            if(yoff < 0)
-  			      yoff = 0;
-
-printf("Image %4d %4d   at %5f %4d   mag %f %f\n",
-   width, height, xoff, yoff, xmag, ymag);
-
-            bool invertBits = colorMap->getDecodeLow(0) == 0.0;
-
-            if (image->isNull()) {
-#if 0
-                  int pw = state->getPageWidth() * xmag;
-                  pw     = ((pw+31)/32) * 32;
-                  int ph = state->getPageHeight() * ymag;
-#endif
-                  int pw = width;
-                  int ph = height;
-                  pw     = ((pw+31)/32) * 32;
-
-                  *image = QImage(pw, ph, QImage::Format_MonoLSB);
-                  QVector<QRgb> ct(2);
-                  ct[0] = qRgb(255, 255, 255);
-                  ct[1] = qRgb(0, 0, 0);
-                  image->setColorTable(ct);
-                  image->fill(0);
-                  ny = yoff;
-printf("  bytes %d\n", (pw * ph) / 8);
-                  }
-
-            // copy the stream
-            str->reset();     // initialize stream
-
-            if (yoff != ny)
-                  printf("  ***next image does not fit, gap %d\n", yoff - ny);
-            yoff = ny;
-
-            int stride  = (width + 7) / 8;
-//            uchar mask  = 0xff << (stride * 8 - width);
-            int qstride = image->bytesPerLine();
-            uchar* p    = image->bits();
-
-printf("Image stride %d qstride %d yoff %d  bytes %d\n",
-   stride, qstride, yoff, height * stride);
-
-            for (int y = 0; y < height; ++y) {
-                  p = image->scanLine(y + yoff);
-                  for (int x = 0; x < stride; ++x) {
-                        static unsigned char invert[16] = {
-                              0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15
-                              };
-                        uchar c = str->getChar();
-                        if (invertBits)
-                              c = ~c;
-                        *p++ = (invert[c & 15] << 4) | invert[(c >> 4) & 15];
-                        }
-//TODO                  p[-1] &= ~mask;
-                  }
-            str->close();
-            ny += height;
-            }
-      else {
-printf("Color Image ================%d %d - %d %d\n", width, height,
-   colorMap->getNumPixelComps(), colorMap->getBits());
-#if 0
-            fprintf(f, "P6\n");
-            fprintf(f, "%d %d\n", width, height);
-            fprintf(f, "255\n");
-
-            // initialize stream
-            ImageStream* imgStr = new ImageStream(str, width, colorMap->getNumPixelComps(), colorMap->getBits());
-            imgStr->reset();
-
-            // for each line...
-            for (int y = 0; y < height; ++y) {
-
-                  // write the line
-                  Guchar *p = imgStr->getLine();
-                  for (int x = 0; x < width; ++x) {
-                        GfxRGB rgb;
-                        colorMap->getRGB(p, &rgb);
-                        fputc(colToByte(rgb.r), f);
-                        fputc(colToByte(rgb.g), f);
-                        fputc(colToByte(rgb.b), f);
-                        p += colorMap->getNumPixelComps();
-                        }
-                  }
-#endif
-            }
-      }
+bool Pdf::init = false;
+static fz_context* ctx;
+static fz_glyph_cache* cache;
 
 //---------------------------------------------------------
 //   numPages
@@ -200,7 +34,7 @@ printf("Color Image ================%d %d - %d %d\n", width, height,
 
 int Pdf::numPages() const
       {
-      return _doc->getNumPages();
+      return pdf_count_pages(xref);
       }
 
 //---------------------------------------------------------
@@ -209,10 +43,18 @@ int Pdf::numPages() const
 
 Pdf::Pdf(const QString& path)
       {
-      globalParams        = new GlobalParams();
-      GooString* fileName = new GooString(qPrintable(path));
-      _doc = new PDFDoc(fileName, 0, 0);
-      imgOut = 0;
+      if (!init) {
+            init = true;
+            ctx = fz_new_context(&fz_alloc_default, 256 << 20);
+            if (!ctx) {
+                  printf("Pdf::init ctx failed\n");
+                  return;
+                  }
+            cache = fz_new_glyph_cache(ctx);
+            }
+      char* name = path.toAscii().data();
+      xref = pdf_open_xref(ctx, name, 0);
+      pdf_load_page_tree(xref);
       }
 
 //---------------------------------------------------------
@@ -221,12 +63,54 @@ Pdf::Pdf(const QString& path)
 
 QImage Pdf::page(int i)
       {
-      if (imgOut == 0)
-            imgOut = new QImageOutputDev();
-      QImage image;
-      imgOut->setImage(&image);
-      // useMediaBox, crop, printing
-      _doc->displayPage(imgOut, i+1, 1200.0, 1200.0, 0, gTrue, gFalse, gFalse);
+      pdf_page* page = pdf_load_page(xref, i);
+      if (page == 0) {
+            printf("cannot load page %d\n", i);
+            return QImage();
+            }
+      static const float resolution = 300.0;
+      const float zoom = resolution / 72.0;
+
+      fz_matrix ctm  = fz_translate(0, -page->mediabox.y1);
+      ctm            = fz_concat(ctm, fz_scale(zoom, -zoom));
+      ctm            = fz_concat(ctm, fz_rotate(page->rotate));
+      fz_bbox bbox   = fz_round_rect(fz_transform_rect(ctm, page->mediabox));
+      fz_pixmap* pix = fz_new_pixmap_with_rect(ctx, fz_device_gray, bbox);
+
+      fz_clear_pixmap_with_color(pix, 255);
+
+      fz_device* dev = fz_new_draw_device(ctx, cache, pix);
+      pdf_run_page(xref, page, dev, ctm);
+      fz_free_device(dev);
+
+      int w = pix->w;
+      int h = pix->h;
+
+      QImage image(w, h, QImage::Format_Mono);
+      QVector<QRgb> ct(2);
+      ct[0] = qRgb(255, 255, 255);
+      ct[1] = qRgb(0, 0, 0);
+      image.setColorTable(ct);
+
+      uchar* s   = pix->samples;
+      int stride = image.bytesPerLine();
+      int bytes  = w >> 3;
+      for (int line = 0; line < h; ++line) {
+            uchar* d = image.bits() + stride * line;
+            for (int col = 0; col < bytes; ++col) {
+                  uchar data = 0;
+                  for (int i = 0; i < 8; ++i) {
+                        uchar v = *s++;
+                        s++;
+                        data <<= 1;
+                        if (v < 128)
+                              data |= 1;
+                        }
+                  *d++ = data;
+                  }
+            }
+      fz_drop_pixmap(ctx, pix);
+      pdf_free_page(ctx, page);
       return image;
       }
 
