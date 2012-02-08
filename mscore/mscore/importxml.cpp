49918d160daaea59714c9e8c64e4cbd9c05b15b6
Nicolas Froment <lasconic@users.noreply.github.com>
2012-02-08 14:30:12 +0000

    fix #14832: import error: 41d-StaffGroups-Nested.xml: part-group symbol=line not supported

--- mscore/mscore/importxml.cpp
+++ mscore/mscore/importxml.cpp
@@ -1043,6 +1043,8 @@ static void partGroupStart(MusicXmlPartGroup* (&pgs)[MAX_PART_GROUPS], int n, in
             bracketType = BRACKET_AKKOLADE;
       else if (s == "bracket")
             bracketType = BRACKET_NORMAL;
+      else if (s == "line")
+            bracketType = BRACKET_NORMAL;
       else {
             qDebug("part-group symbol=%s not supported", s.toLatin1().data());
             return;
