4eecf4b2766ed95e442b31b4267157cb2c5a9b11
lasconic <lasconic@5067e815-4148-4932-9fe1-61ad4153a357>
2012-02-20 22:01:29 +0000

    export MIDI with relative tempo set in Play Panel

--- trunk/mscore/mscore/exportmidi.cpp
+++ trunk/mscore/mscore/exportmidi.cpp
@@ -206,6 +206,7 @@ void ExportMidi::writeHeader()
       //--------------------------------------------
 
       TempoMap* tempomap = cs->tempomap();
+      int relTempo = tempomap->relTempo();      
       foreach(const RepeatSegment* rs, *cs->repeatList()) {
             int startTick  = rs->tick;
             int endTick    = startTick + rs->len;
@@ -219,7 +220,7 @@ void ExportMidi::writeHeader()
                   //
                   // compute midi tempo: microseconds / quarter note
                   //
-                  int tempo = lrint((1.0 / it->second.tempo) * 1000000.0);
+                  int tempo = lrint((1.0 / (it->second.tempo * relTempo * 0.01)) * 1000000.0);
 
                   ev.setMetaType(META_TEMPO);
                   ev.setLen(3);
